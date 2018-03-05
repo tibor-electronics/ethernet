30c976aa64b575ae183f824401be263a4b09a0ab
Kevin Lindsey <kevin@kevlindev.com>
2018-03-05 10:02:59 -0800

    Switch UDP Datagrame to new buffer system

--- ethernet/udp_datagram.py
+++ ethernet/udp_datagram.py
@@ -1,11 +1,24 @@
 # https://en.wikipedia.org/wiki/User_Datagram_Protocol#Packet_structure
 
 class UdpDatagram:
-    def __init__(self, datagram):
-        self.source_port = datagram[0] * 256 + datagram[1]
-        self.destination_port = datagram[2] * 256 + datagram[3]
-        self.length = datagram[4] * 256 + datagram[5]
-        self.checksum = datagram[6] * 256 + datagram[7]
+    @classmethod
+    def from_buffer(cls, buf):
+        return cls(
+           source_port=buf[0] * 256 + buf[1],
+           destination_port=buf[2] * 256 + buf[3],
+           length=buf[4] * 256 + buf[5],
+           checksum=buf[6] * 256 + buf[7],
+           payload=buf[8:]
+        )
+
+
+    def __init__(self, source_port=0, destination_port=0, length=0, checksum=0, payload=None):
+        self.source_port = source_port
+        self.destination_port = destination_port
+        self.length = length
+        self.checksum = checksum
+        self.payload = payload
+
 
     def __repr__(self):
         parts = [
@@ -14,6 +27,7 @@ class UdpDatagram:
             "dst port = {}".format(self.destination_port),
             "length   = {}".format(self.length),
             "checksum = {}".format(self.checksum),
+            "payload  = {}".format(self.payload)
         ]
 
         return "\n  ".join(parts)
